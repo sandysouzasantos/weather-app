30b5fa606f8386b01912c4cfe186a5b9d52c8aa2
Sandy <sandy.santos@capgemini.com>
2018-01-03 17:48:41 -0200

    Add app.js file

---
+++ app.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+let encodedAddress = encodeURIComponent(argv.a);
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyAdsGVUv3tAj7zQAr6hIEobRxW-U-OtDKk`,
+    json: true,
+}, (error, response, body) => {
+
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+
+});
